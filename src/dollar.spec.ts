58019c7c9f24c19f5f5e311c9bac56f6e294971a
elvinciqueira <elvinciqueira@gmail.com>
2022-01-07 10:02:13 -0300

    test: make 'amount' private

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -4,10 +4,8 @@ describe('Dollar', () => {
   describe('when five dollars are multiplied by two', () => {
     it('then returns ten dollars', () => {
       const five: Dollar = new Dollar(5);
-      let product: Dollar = five.times(2);
-      expect(product.amount).toEqual(10);
-      product = five.times(3);
-      expect(product.amount).toEqual(15);
+      expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
+      expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
     });
   });
 
