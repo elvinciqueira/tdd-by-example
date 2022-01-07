5580c7807c8d53551e3e71509f39ce267595bd47
elvinciqueira <elvinciqueira@gmail.com>
2022-01-07 10:05:38 -0300

    test: improve test cases

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,10 +1,16 @@
 import Dollar from './Dollar';
 
 describe('Dollar', () => {
-  describe('when five dollars are multiplied by two and multiplied by three', () => {
-    it('then returns 10 and 15 dollars', () => {
+  describe('when five dollars are multiplied by two', () => {
+    it('then returns 10 dollars', () => {
       const five: Dollar = new Dollar(5);
       expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
+    });
+  });
+
+  describe('when five dollars are multiplied by three', () => {
+    it('then returns 15 dollars', () => {
+      const five: Dollar = new Dollar(5);
       expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
     });
   });
