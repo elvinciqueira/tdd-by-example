cba1531c0adb74677cf33e34d6883489da324445
elvinciqueira <elvinciqueira@gmail.com>
2022-01-06 21:13:47 -0300

    test: remove dollar side effect

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -3,9 +3,11 @@ import Dollar from './Dollar';
 describe('Dollar', () => {
   describe('when five dollars are multiplied by two', () => {
     it('then returns ten dollars', () => {
-      const five = new Dollar(5);
-      five.times(2);
-      expect(five.amount).toBe(10);
+      const five: Dollar = new Dollar(5);
+      let product: Dollar = five.times(2);
+      expect(product.amount).toEqual(10);
+      product = five.times(3);
+      expect(product.amount).toEqual(15);
     });
   });
 });
