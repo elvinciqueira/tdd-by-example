a6adf692d981949245ca4845099925fe01f44c50
elvinciqueira <elvinciqueira@gmail.com>
2022-01-07 10:40:40 -0300

    refactor: make equals a money operation

--- src/Euro.ts
+++ src/Euro.ts
@@ -1,15 +1,12 @@
-export default class Euro {
-  private readonly amount: number;
+import Money from './Money';
 
+export default class Euro extends Money {
   constructor(amount: number) {
-    this.amount = amount;
+    super();
+    super.amount = amount;
   }
 
   times(multiplier: number): Euro {
     return new Euro(this.amount * multiplier);
-  }
-
-  equals(other: Euro): boolean {
-    return this.amount === other.amount;
   }
 }
