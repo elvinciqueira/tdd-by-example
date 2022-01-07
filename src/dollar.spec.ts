2ed94d8d2e1b7b6cff6a4919c78db0e61c4deb26
elvinciqueira <elvinciqueira@gmail.com>
2022-01-07 10:04:03 -0300

    test: change test description

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,8 +1,8 @@
 import Dollar from './Dollar';
 
 describe('Dollar', () => {
-  describe('when five dollars are multiplied by two', () => {
-    it('then returns ten dollars', () => {
+  describe('when five dollars are multiplied by two and multiplied by three', () => {
+    it('then returns 10 and 15 dollars', () => {
       const five: Dollar = new Dollar(5);
       expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
       expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
@@ -10,7 +10,7 @@ describe('Dollar', () => {
   });
 
   describe('when dollars of the same value are compared', () => {
-    it('returns true', () => {
+    it('then returns true', () => {
       expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
       expect(new Dollar(5).equals(new Dollar(10))).toBeFalsy();
     });
