9a3a4fe11ebc40fa1ee7a9544846c681348692d3
elvinciqueira <elvinciqueira@gmail.com>
2022-01-12 20:48:25 -0300

    test: compare euro with dolars

--- src/Money.ts
+++ src/Money.ts
@@ -2,6 +2,8 @@ export default class Money {
   public amount: number;
 
   equals(other: Money): boolean {
+    if (this.constructor !== other.constructor) return false;
+
     return this.amount === other.amount;
   }
 }
--- src/money.spec.ts
+++ src/money.spec.ts
@@ -2,49 +2,33 @@ import Dollar from './Dollar';
 import Euro from './Euro';
 
 describe('Money', () => {
-  describe('dollars', () => {
-    describe('when five dollars are multiplied by two', () => {
-      it('then returns 10 dollars', () => {
-        const five: Dollar = new Dollar(5);
-        expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
-      });
-    });
-
-    describe('when five dollars are multiplied by three', () => {
-      it('then returns 15 dollars', () => {
+  describe('Dollar', () => {
+    describe('with times() called', () => {
+      it('then handles multiplication', () => {
         const five: Dollar = new Dollar(5);
         expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
-      });
-    });
-
-    describe('when dollars of the same value are compared', () => {
-      it('then returns true', () => {
-        expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-        expect(new Dollar(5).equals(new Dollar(10))).toBeFalsy();
+        expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
       });
     });
   });
 
-  describe('euros', () => {
-    describe('when five euros are multiplied by two', () => {
-      it('then returns 10 euros', () => {
+  describe('Euros', () => {
+    describe('with times() called', () => {
+      it('then handles multiplication', () => {
         const five: Euro = new Euro(5);
+        expect(five.times(3).equals(new Euro(15))).toBeTruthy();
         expect(five.times(2).equals(new Euro(10))).toBeTruthy();
       });
     });
+  });
 
-    describe('when five euros are multiplied by three', () => {
-      it('then returns 15 euros', () => {
-        const five: Euro = new Euro(5);
-        expect(five.times(3).equals(new Euro(15))).toBeTruthy();
-      });
-    });
-
-    describe('when euros of the same value are compared', () => {
-      it('then returns true', () => {
-        expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
-        expect(new Euro(5).equals(new Euro(10))).toBeFalsy();
-      });
+  describe('with equals() called', () => {
+    it('then handles money equality', () => {
+      expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
+      expect(new Euro(5).equals(new Euro(10))).toBeFalsy();
+      expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+      expect(new Dollar(5).equals(new Dollar(10))).toBeFalsy();
+      expect(new Dollar(5).equals(new Euro(5))).toBeFalsy();
     });
   });
 });
