159052561a58a7abe24366c8bfbd66d3dd700970
elvinciqueira <elvinciqueira@gmail.com>
2022-01-12 21:39:47 -0300

    test: remove redundent tests

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,21 +1,13 @@
 import { Money } from './Money';
 
 describe('Money', () => {
-  it('when dollar then handles multiplication', () => {
+  it('should handle money multiplication', () => {
     const five: Money = Money.dollar(5);
     expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
     expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
   });
 
-  it('when euros then handles multiplication', () => {
-    const five: Money = Money.euro(5);
-    expect(five.times(3).equals(Money.euro(15))).toBeTruthy();
-    expect(five.times(2).equals(Money.euro(10))).toBeTruthy();
-  });
-
-  it('should handles money equality', () => {
-    expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
-    expect(Money.euro(5).equals(Money.euro(10))).toBeFalsy();
+  it('should handle money equality', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
     expect(Money.dollar(5).equals(Money.dollar(10))).toBeFalsy();
     expect(Money.dollar(5).equals(Money.euro(5))).toBeFalsy();
