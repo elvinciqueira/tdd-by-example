360b3e853bea04417261e4727dadf688b9237b23
elvinciqueira <elvinciqueira@gmail.com>
2022-01-12 21:06:16 -0300

    test: create factory methods dollars and euros

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,34 +1,33 @@
-import Dollar from './Dollar';
-import Euro from './Euro';
+import { Money } from './Money';
 
 describe('Money', () => {
-  describe('Dollar', () => {
+  describe('when dollars', () => {
     describe('with times() called', () => {
       it('then handles multiplication', () => {
-        const five: Dollar = new Dollar(5);
-        expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
-        expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
+        const five: Money = Money.dollars(5);
+        expect(five.times(3).equals(Money.dollars(15))).toBeTruthy();
+        expect(five.times(2).equals(Money.dollars(10))).toBeTruthy();
       });
     });
   });
 
-  describe('Euros', () => {
+  describe('when euros', () => {
     describe('with times() called', () => {
       it('then handles multiplication', () => {
-        const five: Euro = new Euro(5);
-        expect(five.times(3).equals(new Euro(15))).toBeTruthy();
-        expect(five.times(2).equals(new Euro(10))).toBeTruthy();
+        const five: Money = Money.euro(5);
+        expect(five.times(3).equals(Money.euro(15))).toBeTruthy();
+        expect(five.times(2).equals(Money.euro(10))).toBeTruthy();
       });
     });
   });
 
   describe('with equals() called', () => {
     it('then handles money equality', () => {
-      expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
-      expect(new Euro(5).equals(new Euro(10))).toBeFalsy();
-      expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-      expect(new Dollar(5).equals(new Dollar(10))).toBeFalsy();
-      expect(new Dollar(5).equals(new Euro(5))).toBeFalsy();
+      expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
+      expect(Money.euro(5).equals(Money.euro(10))).toBeFalsy();
+      expect(Money.dollars(5).equals(Money.dollars(5))).toBeTruthy();
+      expect(Money.dollars(5).equals(Money.dollars(10))).toBeFalsy();
+      expect(Money.dollars(5).equals(Money.euro(5))).toBeFalsy();
     });
   });
 });
