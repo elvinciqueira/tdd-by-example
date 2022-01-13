190bee789e271269925eb6ec065a9e33d6ad3897
elvinciqueira <elvinciqueira@gmail.com>
2022-01-12 21:23:39 -0300

    test: currency and common constructor

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,33 +1,28 @@
 import { Money } from './Money';
 
 describe('Money', () => {
-  describe('when dollars', () => {
-    describe('with times() called', () => {
-      it('then handles multiplication', () => {
-        const five: Money = Money.dollars(5);
-        expect(five.times(3).equals(Money.dollars(15))).toBeTruthy();
-        expect(five.times(2).equals(Money.dollars(10))).toBeTruthy();
-      });
-    });
+  it('when dollar then handles multiplication', () => {
+    const five: Money = Money.dollar(5);
+    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
+    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
   });
 
-  describe('when euros', () => {
-    describe('with times() called', () => {
-      it('then handles multiplication', () => {
-        const five: Money = Money.euro(5);
-        expect(five.times(3).equals(Money.euro(15))).toBeTruthy();
-        expect(five.times(2).equals(Money.euro(10))).toBeTruthy();
-      });
-    });
+  it('when euros then handles multiplication', () => {
+    const five: Money = Money.euro(5);
+    expect(five.times(3).equals(Money.euro(15))).toBeTruthy();
+    expect(five.times(2).equals(Money.euro(10))).toBeTruthy();
   });
 
-  describe('with equals() called', () => {
-    it('then handles money equality', () => {
-      expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
-      expect(Money.euro(5).equals(Money.euro(10))).toBeFalsy();
-      expect(Money.dollars(5).equals(Money.dollars(5))).toBeTruthy();
-      expect(Money.dollars(5).equals(Money.dollars(10))).toBeFalsy();
-      expect(Money.dollars(5).equals(Money.euro(5))).toBeFalsy();
-    });
+  it('should handles money equality', () => {
+    expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
+    expect(Money.euro(5).equals(Money.euro(10))).toBeFalsy();
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+    expect(Money.dollar(5).equals(Money.dollar(10))).toBeFalsy();
+    expect(Money.dollar(5).equals(Money.euro(5))).toBeFalsy();
+  });
+
+  it('should handle currencies', () => {
+    expect(Money.dollar(5).currency()).toBe('USD');
+    expect(Money.euro(5).currency()).toBe('EUR');
   });
 });
